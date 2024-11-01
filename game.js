e22f64fefdb20afb08b1fd97a5dadc966e5c08dd
BrianKamauKahara <167960457+BrianKamauKahara@users.noreply.github.com>
2024-11-01 13:10:03 +0300

    When coin collected, it moves to a new location, and the hazards are regenerated.  This works flawlessy, but, the code is unoptimal :(

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const player = document.getElementById('player'),
 
 
 // Giving the player height and width
-player.style.height = player.style.width = '40px'
+player.style.height = player.style.width = '30px'
 
 
 const bodyHeight = Number(computedBody.height.split('p')[0]),
@@ -17,6 +17,9 @@ const bodyHeight = Number(computedBody.height.split('p')[0]),
 
 let playing = false
 
+
+/* ---------SECTION 1: PLAYER MOVEMENT FUNCTIONALITY  */
+// DEFINITION OF GLOBAL STATE VARIABLES
 const keyStates = {
     'w':false,
     'd':false,
@@ -32,7 +35,7 @@ const playerCoordinates = {
     'X':[0,playerWidth/distance]
 }
 
-  
+// W A S D MOVEMENT EVENT LISTENER
 player.addEventListener('keypress', (e) => {
     if (!playing) {return}
     key = e.key.toLowerCase()
@@ -58,7 +61,7 @@ player.addEventListener('keypress', (e) => {
     keyStates[key] = true
 })
 
-
+// W A S D STOPPING MOVEMENT EVENT LISTENER
 player.addEventListener('keyup', (e) => {
     if (!playing) {return}
     key = e.key.toLowerCase()
@@ -72,12 +75,14 @@ player.addEventListener('keyup', (e) => {
     keyStates[key] = false
 })
 
-
+// ACTUAL MOVEMENT FUNTIONALITY
+let interval;
 function update(direction) {
     /* If you are not to update in a certain direction, as indicated by the 
     keyStates dictionary, then do not*/
     if (!keyStates[direction]) {return}
     
+
     // Determine the translation based on the direction of motion
 
     /*
@@ -108,7 +113,7 @@ function update(direction) {
             playerCoordinates['Y'][0]++
             playerCoordinates['Y'][1]++
             player.style.top = -distance*playerCoordinates['Y'][0] + 'px'
-            checkPlayerAlive()
+            
             break
         case 's':
             // Positions the player ten pixels down
@@ -116,7 +121,7 @@ function update(direction) {
             playerCoordinates['Y'][0]--
             playerCoordinates['Y'][1]--
             player.style.top = -distance*playerCoordinates['Y'][0] + 'px'
-            checkPlayerAlive()
+            
             break
         case 'd':
             // Positions the player ten pixels right
@@ -124,7 +129,7 @@ function update(direction) {
             playerCoordinates['X'][0]++
             playerCoordinates['X'][1]++
             player.style.left = distance*playerCoordinates['X'][0] + 'px'
-            checkPlayerAlive()
+            
             break
         case 'a':
             // Positions the player ten pixels left
@@ -132,21 +137,23 @@ function update(direction) {
             playerCoordinates['X'][0]--
             playerCoordinates['X'][1]--
             player.style.left = distance*playerCoordinates['X'][0] + 'px'
-            checkPlayerAlive()
+            
             break
     }
+    // After the movement, check whether the player is alive, and whether they got the coin
+    checkPlayerAlive()
+    checkPlayerGotCoin('one-chase')
 }
 
-// This function returns the player's position
-function playerCurrentXY () {
-    return [playerCoordinates['X'],playerCoordinates['Y']]
+// stops the player from moving
+function stationPlayer () {
+    // This function stops the program from updating the player's position
+    clearInterval(interval)
 }
 
-
-
-// This function tells the program to continuously update the player's position
-let interval;
-const updatePlayer = function() {
+// enables all movement functionality to start
+function enablePlayerMovement() {
+    // This function keeps the program updating the player
     playing = true
     interval = setInterval(() => {
         update('w')
@@ -156,12 +163,13 @@ const updatePlayer = function() {
     },speed)
 }
 
-// This function stops the program from updating the player's position
-const stationPlayer = function() {
-    clearInterval(interval)
+
+// THIS FUNCTION RETURNS THE PLAYER'S CURRENT POSITION --- FOR CONVENIENT USE
+function playerCurrentXY () {
+    return [playerCoordinates['X'],playerCoordinates['Y']]
 }
 
-// This function displays the players position every second
+// THIS FUNCTION DISPLAYS THE PLAYER'S POSITION EVERY SECOND --- FOR CONVENIENCE
 function displayPlayerPosition(){
     setInterval(function () {
         /* console.log(...playerCurrentXY()) */
@@ -169,55 +177,45 @@ function displayPlayerPosition(){
     }, 1000)
 }
 
-
-// HAZARDS / HAZARD AREAS / GAME END AREAS
+/* ---------SECTION 2: PLACEMENT OF HAZARDS AND COINS ON THE GAME BOARD  */
+// DEFINITION OF GLOBAL STATE VARIABLES
 let widthDivisions = 16, 
     heightDivisions = 12,
     ratioHazards = 0.4,
     numGrids = widthDivisions * heightDivisions
 
 
-let square, numHazards, gridWidth, gridHeight
+let board, numHazards, gridWidth, gridHeight
 
-
+gridWidth = bodyWidth/widthDivisions
+gridHeight = bodyHeight/heightDivisions
 
 let Hazards = [],
     hazardData = {},
     loadedHazards = [],
-    loadHazards,
-    coins = [],
+    loadBoardInterval,
+    coinRegions = [],
     coinsPlaced = false,
-    coinString
+    coinString,
+    hazardDivs,
+    coinDivs
+let gotCoin = false
 
+// THIS FUNCTION CALCULATES AND STORES DATA ON THE AREAS WHERE HAZARDS WILL BE
 function generateHazardAreas(){
     Hazards = []
     hazardData = {}
 
-    // This is the hazard area divided into a grid. 
-    square = Array.from({ length: numGrids }, (_, index) => index + 1)
-
     // This is the number of hazards, given by the ratio provided
     numHazards = Math.floor(numGrids * ratioHazards) + 1
 
-    // This is the width and height of the hazards
-    gridWidth = bodyWidth/widthDivisions
-    gridHeight = bodyHeight/heightDivisions
-
-
-    // First, generate the coins that he user is to collect
-    if (!coinsPlaced){
-        generateCoins('one-chase')
-    } else {
-        square.splice(square.indexOf(coins[0]), 1)
-    }
-    
-
     // Generate the hazards at random places. For now, this process is random
+    /* let fullBoard = [...board] */
     for (let i =0; i < numHazards; i++){
         // Get that random place
-        Hazards.push(...square.splice(Math.floor(Math.random() * (numGrids - i + 1)),1))
+        Hazards.push(...board.splice(Math.floor(Math.random() * (numGrids)- i + 1),1))
     }
-
+    /* board = fullBoard */
     // Going through each hazard
     Hazards.forEach((hazard) => {
 
@@ -232,10 +230,9 @@ function generateHazardAreas(){
             }
         }
     })
-
-    console.log(Hazards)
 }
 
+// THIS FUNCTION CONVERTS GRID NUMBER (REGIONAL CO-ORDS) TO PIXEL POSITION
 function determinePosition (gridNo) {
     // Determine the row AND column
     let row = Math.floor((gridNo - 1) / widthDivisions) + 1
@@ -243,76 +240,136 @@ function determinePosition (gridNo) {
 
     let gridX = (column - 1) * gridWidth
     let gridY = (row - 1) * gridHeight
-    return [gridX, gridY]
+    return [Number(gridX), Number(gridY)]
 }
 
-/* console.log('Hazard Width',gridWidth,'::','Hazard Height',gridHeight) */
+// THIS FUNCTION PLACES THE HAZARDS ON THE GAME BOARD
 function placeHazards(){
+    /* These are the loaded hazards. 
+    Yes, though the hazards are place immediately as this function  proceeds, 
+    they first go through an animation as if they are 'loading-ing' to give 
+    the player time to escape. Then, once that time period passes, the loadedHazards
+    array is set equal to the hazards list, so that the checkPlayerAlive function
+    (which checks whether the player's regions are in one of the hazard's regions),
+    which uses the loadedHazards list, will function well now
+    */
     loadedHazards = []
     // This function places the hazards in the game area
-    let divs = ''
     
     Hazards.forEach((hazard) => {
         let { X: haz_X, Y :haz_Y } = hazardData[hazard]["placement"]
         
         // Each hazard's start location (X and Y) is given as already calculated
-        divs += `
-        <div class="hazard" id="haz_${hazard}" style = "width:${gridWidth}px; height:${gridHeight}px; position: fixed; top:${haz_Y}px; left:${haz_X}px;"></div>
+        hazardDivs += `
+        <div class="hazard tile animate" id="haz_${hazard}" style = "width:${gridWidth}px; height:${gridHeight}px; position: fixed; top:${haz_Y}px; left:${haz_X}px; animation: loading-hazard 1s ease 0s 1 forwards;"></div>
         `
     })
 
-    if (!coinsPlaced) {
-        placeCoins('one-chase')   
-    }
-    divs += coinString
-    
-
-    gameBoard.innerHTML = divs
-    
-
-    
-    
-    setTimeout(() => {
-        loadedHazards = Hazards
-        checkPlayerAlive()
-    }, 1010)
-    
+    gameBoard.innerHTML += hazardDivs 
 }
 
-function generateCoins(mode) {
+// THIS FUNCTION CALCULATES AND STORES DATA ON THE AREAS WHERE THE COINS WILL BE
+function generateCoinAreas(mode) {
+    /* 
+    If the coin has already been placed, the program updates the board, so that 
+    when generating the hazards, a hazard does not get placed where a coin is placed
+    */
+   console.log(coinsPlaced)
+    if (coinsPlaced) {
+        coinRegions.forEach((coin) => {
+            board.splice(coin-1, 1)
+        })
+        coinDivs = ''
+        return
+    }
     switch (mode){
         case 'one-chase':
-            coins.push(...square.splice(Math.floor(Math.random() * (numGrids + 1)),1))
+            // Place the coin on one random tile
+            
+            coinDivs = ''
+            let num = Math.floor(Math.random() *board.length + 1)
+            let pick = board.splice(num - 1,1)
+            
+            /* checkIfInBoard(pick) */
+            coinRegions = []
+            coinRegions.push(...pick)
+            coinsPlaced = true
+            console.log(num,pick,coinRegions)
     }
 }
 
+// THIS FUNCTION PLACES THE COINS ON THE GAMEBOARD
 function placeCoins(mode) {
-    coinsPlaced = true
+    /* Then, the global state coinsPlaced is set to true, so that coins are not generated
+    again in the generated  */
+    
     switch (mode) {
         case 'one-chase':
-            const [gridX, gridY] = determinePosition(coins[0])
-            coinString = `<div class="coin" id="coin_1" style="width: ${gridWidth}px; height: ${gridHeight}px; position: fixed; top: ${gridY}px; left: ${gridX}px;"></div>`
+            const [gridX, gridY] = determinePosition(coinRegions[0])
+            coinDivs = `<div class="coin tile" id="coin_1" style="width: ${gridWidth}px; height: ${gridHeight}px; position: fixed; top: ${gridY}px; left: ${gridX}px;"></div>`
     }
+    gameBoard.innerHTML += coinDivs
 }
 
-function generateHazards() {
-    loadHazards = setInterval(() => {
-        generateHazardAreas()
-        placeHazards()
-        
+// THIS FUNCTION REPEATEDLY GENERATES THE HAZARDS AND COINS
+function generateBoard(mode) {
+        switch (mode){
+            case 'one-chase':
+                loadBoard()
+
+                generateCoinAreas(mode)
+                generateHazardAreas()
+
+                
+                placeCoins(mode)
+
+                hazardDivs = ''
+                placeHazards()
+
+                setTimeout(() => {
+                    loadedHazards = Hazards
+                    checkPlayerAlive()
+                }, 1210)
+                break
+        }
+
+    loadBoardInterval = setInterval(() => {
+        console.log('ROUND')
+        switch (mode){
+            case 'one-chase':
+                loadBoard()
+
+                generateCoinAreas(mode)
+                generateHazardAreas()
+
+                
+                placeCoins(mode)
+
+                hazardDivs = ''
+                placeHazards()
+
+                setTimeout(() => {
+                    loadedHazards = Hazards
+                    checkPlayerAlive()
+                }, 1210)
+                break
+        }     
     }, 3000);
 }
+function loadBoard() {
+    board = Array.from({ length: numGrids }, (_, index) => index + 1)
+    gameBoard.innerHTML = ''
+}
 
-
+// THIS FUNCTION GETS THE REGIONAL CO-RDS WHICH THE PLAYER IS IN -- FOR UTMOST CONVENIENCE
 function getPlayerRegions (){
     let playerCoords = playerCurrentXY()
-    //console.log(playerCoords)
 
     /* 
     This function is necessary to check whether the player has entered 
     a region where there is a hazard. 
     This is done by getting the regions the player is in (because the player
-    is a square, it is sufficient to check only the player's corners) 
+    is a board, it is sufficient to check only the player's corners) 
     
     This is the purpose of the function:
     To convert the player's co-ordinates to regional co-ordinates 
@@ -364,7 +421,7 @@ function getPlayerRegions (){
     return regions
 }
 
-
+// THIS FUNCTION CHECKS WHETHER THE PLAYER HAS STRAYED INTO A HAZARD
 function checkPlayerAlive () {
     /* 
     This function checks whether the player is alive, by checking whether
@@ -385,6 +442,47 @@ function checkPlayerAlive () {
     return alive
 }
 
+// This function checks whether the provided region is within the board
+function checkIfInBoard (num) {
+    for (let i = 0; i < 192; i++){
+        if (!board.includes(i+1)) {
+            console.log('AAAAA',num, i+1)
+        }
+    }
+    console.log(Hazards)
+}
+
+// THIS FUNCTION CHECKS WHETHER THE PLAYER HAS GOTTEN TO A COIN
+function checkPlayerGotCoin (mode) {
+    if (gotCoin) {return}
+    const playerRegions = getPlayerRegions()
+    
+
+    switch (mode) {
+        case 'one-chase':
+            playerRegions.forEach((region) => {
+                if (coinRegions.includes(region)) {
+                    gotCoin = true
+                }
+            })
+            if (gotCoin) {
+                coinsPlaced = false
+                document.querySelector('.coin').remove()
+                
+                loadedHazards = []
+                clearInterval(loadBoardInterval)
+                generateBoard('one-chase')
+                gotCoin = false
+            }
+            break
+    }
+    
+    
+
+    
+}
+
+// THIS FUNCTION ENDS THE GAME
 function endGame() {
     playing = false
 
@@ -392,13 +490,16 @@ function endGame() {
     stationPlayer()
 
     // Stop loading hazards
-    clearInterval(loadHazards)
+    clearInterval(loadBoardInterval)
 
     console.log('PLAYER DIED')
-    player.classList.toggle('dead-player')
+    player.classList.add('dead-player')
 
 }
 
-updatePlayer()
+
+
+enablePlayerMovement()
 displayPlayerPosition()
-generateHazards()
+generateBoard('one-chase')
+
